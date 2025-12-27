87e998b2782532843a8b0c7ae6910bb6b53eae89
Oliver Baer-Benson <61638242+opbb@users.noreply.github.com>
2025-12-27 00:14:58 -0600

    beginnings of everything

---
+++ main-transpiler.py
@@ -0,0 +1,132 @@
+import os
+import sys
+
+import charTransFuncs
+
+# Defaults
+GROUPS_DIRECTORY = "groups/"
+HEAD_FILE_PATH = "head.html"
+SCRIPT_LINKS_FILE_PATH = "scriptLinks.html"
+CURRENT_DIRECTORY_PATH = os.getcwd()
+charArgIndex, groupArgIndex = -1, -1
+
+HELP_TEXT = "Expected args: backgroundFile -c characterFile1 ... characterFileN -g groupFile1 ... groupFileM\nFile paths should be relative to the current directory."
+for i in range(1, len(sys.argv)):
+    match sys.argv[i]:
+        case "-c":
+            charArgIndex = i
+        case "-g":
+            groupArgIndex = i
+        case "-h":
+            print(HELP_TEXT)
+            exit(0)
+        case "--help":
+            print(HELP_TEXT)
+            exit(0)
+
+if len(sys.argv) < 2:
+    print("Error: Must include the background layer file as an argument.")
+    exit(-1)
+
+if charArgIndex == 1 or groupArgIndex == 1:
+    print("Error: The first argument must be the background layer file.")
+    exit(-1)
+
+
+# Process command line arguments
+
+bkgFilePath = sys.argv[1]
+
+charFilePaths = None
+if charArgIndex != -1:
+    charFilePaths = sys.argv[
+        (charArgIndex + 1) : (
+            groupArgIndex if groupArgIndex > charArgIndex else len(sys.argv)
+        )
+    ]
+    if len(charFilePaths) == 0:
+        charFilePaths = None
+
+groupFilePaths = None
+if groupArgIndex != -1:
+    groupFilePaths = sys.argv[
+        (groupArgIndex + 1) : (
+            charArgIndex if charArgIndex > groupArgIndex else len(sys.argv)
+        )
+    ]
+    if len(groupFilePaths) == 0:
+        groupFilePaths = None
+
+
+# Check if given files exist
+
+if not os.path.isfile(bkgFilePath):
+    print('Error: Could not find given file "' + bkgFilePath + '"')
+    exit(-1)
+
+if charFilePaths != None:
+    for charFilePath in charFilePaths:
+        if not os.path.isfile(charFilePath):
+            print('Error: Could not find given file "' + charFilePath + '"')
+            exit(-1)
+
+if groupFilePaths != None:
+    for groupFilePath in groupFilePaths:
+        if not os.path.isfile(groupFilePath):
+            print('Error: Could not find given file "' + groupFilePath + '"')
+            exit(-1)
+
+if not os.path.isdir(GROUPS_DIRECTORY):
+    print(
+        'Error: Could not find the groups directory, which should be named  "'
+        + GROUPS_DIRECTORY
+        + '"'
+    )
+    exit(-1)
+
+if not os.path.isfile(HEAD_FILE_PATH):
+    print(
+        'Error: Could not find the HTML head file, which should be named  "'
+        + HEAD_FILE_PATH
+        + '"'
+    )
+    exit(-1)
+
+if not os.path.isfile(SCRIPT_LINKS_FILE_PATH):
+    print(
+        'Error: Could not find the scripts file, which should be named  "'
+        + SCRIPT_LINKS_FILE_PATH
+        + '"'
+    )
+    exit(-1)
+
+# List containing all layers in order
+layers = []
+currentLayerIndex = 0
+
+# Make a generic table creation function?
+# It seems like all the variety happens in the cells and in the outer table element
+# Pass functions to define those two pieces.
+# Could be fun?
+
+# TODO: Process background
+#       - Define page dimensions
+#       - Create list of row strings
+#       - Create cells, appending them to row string
+#           - Give each cell a bkg color using the style property
+#           - Throw out characters and foreground color
+#           - Give each cell an ID according to this scheme: `id="COLUMN-ROW"`
+#       - Append row strings together
+#       - Add wrapping table HTML with `id="background"`
+
+# TODO: Process character layers
+#       - FOR EACH LAYER
+#       - Create list of row strings
+#       - Create cells, appending them to row string
+#           - Special case: Check for #FFFFFF background color. Remove character if found.
+#           - Add charTransFunc output to cell on background color
+#       - Append row strings together
+#       - Add wrapping table HTML with `class="character-layer" style="z-index: i;"`
+
+# TODO: Process group layers
+#       -
---
+++ scripts/process.js
@@ -0,0 +1,57 @@
+// Linearly interpolates between a and b over time t
+function lerp(a, b, t) {
+  return a + (b - a) * t;
+}
+
+// Lerp between 2d vectors
+function vlerp(vecA, vecB, t) {
+  return {
+    x: lerp(vecA.x, vecB.x, t),
+    y: lerp(vecA.y, vecB.y, t),
+  };
+}
+
+const SCREEN_LAG_MAGIC_NUM = 0.1;
+const TICK_INTERVAL = 33;
+let scrollTarget = { x: 0, y: 0 };
+let scrollPos = { x: 0, y: 0 };
+function _process() {
+  scrollPos = vlerp(scrollPos, scrollTarget, SCREEN_LAG_MAGIC_NUM);
+
+  window.scroll(scrollPos.x, scrollPos.y);
+}
+
+setInterval(_process, TICK_INTERVAL);
+
+let worldContainer = document.getElementById("world-container");
+console.log(
+  "Width, window: " +
+    window.innerWidth +
+    ", world: " +
+    worldContainer.offsetWidth,
+);
+console.log(
+  "Height, window: " +
+    window.innerHeight +
+    ", world: " +
+    worldContainer.offsetHeight,
+);
+
+let mouseX, mouseY;
+document.onmousemove = handleMouseMove;
+function handleMouseMove(event) {
+  event = event || window.event; // IE-ism
+  mouseX = event.pageX;
+  mouseY = event.pageY;
+
+  let widthDiff = worldContainer.offsetWidth - window.innerWidth;
+  let heightDiff = worldContainer.offsetHeight - window.innerHeight;
+
+  let xPercent = mouseX / window.innerWidth;
+  let yPercent = mouseY / window.innerHeight;
+
+  scrollTarget.x = xPercent * widthDiff;
+  scrollTarget.y = yPercent * heightDiff;
+
+  console.log(scrollTarget);
+}
